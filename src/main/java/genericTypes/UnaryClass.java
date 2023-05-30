c19e0e958185d5acd682f9619de00baa0579dd38
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-30 20:07:08 +0200

    added generic singleton with @SuppressWarnings annotation

---
+++ src/main/java/genericTypes/UnaryClass.java
@@ -0,0 +1,24 @@
+package genericTypes;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryClass {
+    public static void main(String[] args) {
+        String[] strings = {"Błażej", "Wojtek", "Tomek"};
+        UnaryOperator<String> sameString = identityFunction();
+        for (String s : strings) {
+            System.out.println(sameString.apply(s));
+        }
+        Number[] numbers = {1, 2.0, 3L};
+        UnaryOperator<Number> sameNumber = identityFunction();
+        for (Number n : numbers) {
+            System.out.println(sameNumber.apply(n));
+        }
+    }
+    public static UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+
+    @SuppressWarnings("unchecked")
+    public static <T> UnaryOperator<T> identityFunction() {
+        return (UnaryOperator<T>) IDENTITY_FN;
+    }
+}
